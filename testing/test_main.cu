8830a97336736251243ca2d5693a5e00e773e8ec
Dominik Soós <soos.domi@gmail.com>
2025-06-10 18:23:10 -0400

    testfunctions

--- testing/test_main.cu
+++ testing/test_main.cu
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
+#include "../parallel_pso.cuh"
+
 // Forward declarations for device functions from main.cu
 extern "C" {
     __device__ void vector_add(const double* a, const double* b, double* result, int size);
     __device__ void vector_scale(const double* a, double scalar, double* result, int dim);
 }
+
 
 // Define __global__ kernels in the test file that call the device functions.
 __global__ void testVectorAddKernel(const double* a, const double* b, double* result, int size) {
@@ -72,10 +75,116 @@ void test_vector_scale() {
     printf("test_vector_scale passed.\n");
 }
 
+template<template<int> class Func, int DIM>
+__global__ void testValueKernel(const double* x, double* out) {
+  out[0] = Func<DIM>::evaluate(x);
+}
+
+template<template<int> class Func, int DIM>
+void run_value_test(const double x[DIM], double expect, const char* name) {
+  double *dX, *dOut, hOut;
+  cudaMalloc(&dX, DIM*sizeof(double));
+  cudaMalloc(&dOut,sizeof(double));
+  cudaMemcpy(dX,x,DIM*sizeof(double),cudaMemcpyHostToDevice);
+  testValueKernel<Func,DIM><<<1,1>>>(dX,dOut);
+  cudaMemcpy(&hOut, dOut,sizeof(double),cudaMemcpyDeviceToHost);
+  assert(fabs(hOut - expect) < 1e-8);
+  cudaFree(dX);
+  cudaFree(dOut);
+  printf("%s (value) passed\n", name);
+}
+
+//------------------------------------------------------------------------------
+// Gradient‐test kernel
+//------------------------------------------------------------------------------
+template<template<int> class Func, int DIM>
+__global__ void testGradKernel(const double* x, double* g) {
+  util::calculateGradientUsingAD<Func<DIM>, DIM>(
+    const_cast<double*>(x), g
+  );
+}
+
+template<template<int> class Func, int DIM>
+void run_grad_test(const double x[DIM], const double expect[DIM], const char* name) {
+  double *dX, *dG, hG[DIM];
+  cudaMalloc(&dX, DIM*sizeof(double));
+  cudaMalloc(&dG, DIM*sizeof(double));
+  cudaMemcpy(dX,x,DIM*sizeof(double),cudaMemcpyHostToDevice);
+  testGradKernel<Func,DIM><<<1,1>>>(dX,dG);
+  cudaMemcpy(hG, dG, DIM*sizeof(double),cudaMemcpyDeviceToHost);
+  for(int i=0;i<DIM;++i)
+    assert(fabs(hG[i] - expect[i]) < 1e-6);
+  cudaFree(dX);
+  cudaFree(dG);
+  printf("%s (grad) passed\n", name);
+}
+
 int main() {
     test_vector_add();
     test_vector_scale();
+
+    {// rastirign
+      const int D = 2;
+      double x0[D] = {0.0,0.0};
+      // at min: f=10*2 + 0 = 20
+      run_value_test<util::Rastrigin,D>(x0, 10.0*D + 0.0, "rastrigin@origin");
+      double x1[D] = {0.1,-0.3};
+      double expected1 = 10.0*D
+        + (0.1*0.1 - 10*cos(2*M_PI*0.1))
+        + ((-0.3)*(-0.3) - 10*cos(2*M_PI*-0.3));
+      run_value_test<util::Rastrigin,D>(x1, expected1, "rastrigin@generic");
+    }
+        // --- Ackley value tests (2D) ---
+    {
+      const int D = 2;
+      double x0[D] = {0.0,0.0};
+      // at origin: f = 0
+      run_value_test<util::Ackley,D>(x0, 0.0, "ackley@origin");
+      double x1[D] = {0.5,-0.5};
+      // compute on host:
+      double sumsq = 0.5*0.5 + (-0.5)*(-0.5);
+      double sumcos= cos(2*M_PI*0.5)+cos(2*M_PI*-0.5);
+      double expected1 = -20*exp(-0.2*sqrt(sumsq/D))
+                       - exp(sumcos/D)
+                       + 20 + M_E;
+      run_value_test<util::Ackley,D>(x1, expected1, "ackley@generic");
+    }
+
+    // --- Rosenbrock value tests (2D) ---
+    {
+      const int D = 2;
+      double x0[D] = {1.0,1.0};
+      // at min: f=0
+      run_value_test<util::Rosenbrock,D>(x0, 0.0, "rosenbrock@min");
+      double x1[D] = {1.2,1.2};
+      double expected1 = 100*(1.2 - 1.2*1.2)*(1.2 - 1.2*1.2)
+                       + (1 - 1.2)*(1 - 1.2);
+      run_value_test<util::Rosenbrock,D>(x1, expected1, "rosenbrock@generic");
+    }
+
+    // --- AD gradient tests (2D) ---
+    {
+      const int D = 2;
+      //--- Rastrigin grad @ origin = [0,0]
+      {
+        double x[D] = {0.0,0.0}, expg[D]={0,0};
+        run_grad_test<util::Rastrigin,D>(x, expg, "grad_rastrigin@origin");
+      }
+      //--- Ackley grad @ origin = [0,0]
+      {
+        double x[D] = {0.0,0.0}, expg[D]={0,0};
+        run_grad_test<util::Ackley,D>(x, expg, "grad_ackley@origin");
+      }
+      //--- Rosenbrock grad @ min = [0,0]
+      {
+        double x[D] = {1.0,1.0}, expg[D]={0,0};
+        run_grad_test<util::Rosenbrock,D>(x, expg, "grad_rosenbrock@min");
+      }
+    }
+
     printf("All tests passed!\n");
+
+
     return 0;
 }
 
