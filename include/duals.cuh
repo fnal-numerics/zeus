15fdbe4dc10191b5dc3810d64fcf42430edeaa09
Marc Paterno <paterno@fnal.gov>
2025-07-08 11:23:50 -0500

    Revert "Apply clang-format"
    This reverts commit 8a866947977686b03688d968d605baa27437e106.

--- include/duals.cuh
+++ include/duals.cuh
@@ -2,132 +2,96 @@
 
 namespace dual {
 
-  class DualNumber {
-  public:
+class DualNumber {
+public:
     double real;
     double dual;
 
-    __host__ __device__
-    DualNumber(double real = 0.0, double dual = 0.0)
-      : real(real), dual(dual)
-    {}
+    __host__ __device__ DualNumber(double real = 0.0, double dual = 0.0) : real(real), dual(dual) {}
 
-    __host__ __device__ DualNumber&
-    operator+=(const DualNumber& rhs)
-    {
-      real += rhs.real;
-      dual += rhs.dual;
-      return *this;
+    __host__ __device__ DualNumber& operator+=(const DualNumber& rhs) {
+        real += rhs.real;
+        dual += rhs.dual;
+        return *this;
     }
 
-    __host__ __device__ DualNumber
-    operator+(const DualNumber& rhs) const
-    {
-      return DualNumber(real + rhs.real, dual + rhs.dual);
+    __host__ __device__ DualNumber operator+(const DualNumber& rhs) const {
+        return DualNumber(real + rhs.real, dual + rhs.dual);
     }
 
-    __host__ __device__ DualNumber
-    operator-(const DualNumber& rhs) const
-    {
-      return DualNumber(real - rhs.real, dual - rhs.dual);
+    __host__ __device__ DualNumber operator-(const DualNumber& rhs) const {
+        return DualNumber(real - rhs.real, dual - rhs.dual);
     }
 
-    __host__ __device__ DualNumber
-    operator*(const DualNumber& rhs) const
-    {
-      return DualNumber(real * rhs.real, dual * rhs.real + real * rhs.dual);
+    __host__ __device__ DualNumber operator*(const DualNumber& rhs) const {
+        return DualNumber(real * rhs.real, dual * rhs.real + real * rhs.dual);
     }
 
-    __host__ __device__ DualNumber
-    operator/(const DualNumber& rhs) const
-    {
-      double denom = rhs.real * rhs.real;
-      return DualNumber(real / rhs.real,
-                        (dual * rhs.real - real * rhs.dual) / denom);
+    __host__ __device__ DualNumber operator/(const DualNumber& rhs) const {
+        double denom = rhs.real * rhs.real;
+        return DualNumber(real / rhs.real, (dual * rhs.real - real * rhs.dual) / denom);
     }
     // operator for double - DualNumber
-    __host__ __device__ friend DualNumber
-    operator-(double lhs, const DualNumber& rhs)
-    {
-      return DualNumber(lhs - rhs.real, -rhs.dual);
+    __host__ __device__ friend DualNumber operator-(double lhs, const DualNumber& rhs) {
+        return DualNumber(lhs - rhs.real, -rhs.dual);
     }
 
     // operator for double * DualNumber
-    __host__ __device__ friend DualNumber
-    operator*(double lhs, const DualNumber& rhs)
-    {
-      return DualNumber(lhs * rhs.real, lhs * rhs.dual);
+    __host__ __device__ friend DualNumber operator*(double lhs, const DualNumber& rhs) {
+        return DualNumber(lhs * rhs.real, lhs * rhs.dual);
     }
-  };
+};
 
-  __host__ __device__ inline dual::DualNumber
-  dual_abs(const dual::DualNumber& a)
-  {
+__host__ __device__ inline dual::DualNumber dual_abs(const dual::DualNumber &a) {
     return (a.real < 0.0) ? dual::DualNumber(-a.real, -a.dual) : a;
-  }
+}
 
-  static __inline__ __host__ __device__ DualNumber
-  sin(const DualNumber& x)
-  {
+static __inline__ __host__ __device__ DualNumber sin(const DualNumber& x) {
     return DualNumber(sinf(x.real), x.dual * cosf(x.real));
-  }
+}
 
-  static __inline__ __host__ __device__ DualNumber
-  cos(const DualNumber& x)
-  {
+static __inline__ __host__ __device__ DualNumber cos(const DualNumber& x) {
     return DualNumber(cosf(x.real), -x.dual * sinf(x.real));
-  }
+}
 
-  static __inline__ __host__ __device__ DualNumber
-  exp(const DualNumber& x)
-  {
+static __inline__ __host__ __device__ DualNumber exp(const DualNumber& x) {
     double ex = expf(x.real);
     return DualNumber(ex, x.dual * ex);
-  }
+}
 
-  static __inline__ __host__ __device__ DualNumber
-  sqrt(const DualNumber& x)
-  {
+static __inline__ __host__ __device__ DualNumber sqrt(const DualNumber& x) {
     double sr = sqrtf(x.real);
     return DualNumber(sr, x.dual / (2.0 * sr));
-  }
+}
 
-  static __inline__ __host__ __device__ DualNumber
-  atan2(const DualNumber& y, const DualNumber& x)
-  {
+static __inline__ __host__ __device__ DualNumber atan2(const DualNumber& y, const DualNumber& x) {
     double denom = x.real * x.real + y.real * y.real;
-    return DualNumber(atan2f(y.real, x.real),
-                      (x.real * y.dual - y.real * x.dual) / denom);
-  }
+    return DualNumber(atan2f(y.real, x.real), (x.real * y.dual - y.real * x.dual) / denom);
+}
 
-  template <typename T>
-  static __inline__ __host__ __device__ T
-  pow(const T& base, double exponent)
-  {
-    return T(powf(base.real, exponent),
-             exponent * powf(base.real, exponent - 1) * base.dual);
-  }
+template<typename T>
+static __inline__ __host__ __device__ T pow(const T& base, double exponent) {
+    return T(powf(base.real, exponent), exponent * powf(base.real, exponent - 1) * base.dual);
+}
 
-  template <typename Function, int DIM>
-  __device__ void
-  calculateGradientUsingAD(double* x, double* gradient)
-  {
+template<typename Function, int DIM>
+__device__ void calculateGradientUsingAD(double *x, double *gradient) {
     dual::DualNumber xDual[DIM];
 
-    for (int i = 0; i < DIM;
-         ++i) { // // iterate through each dimension (vairbale)
-      xDual[i] = dual::DualNumber(x[i], 0.0);
+    for (int i = 0; i < DIM; ++i) { // // iterate through each dimension (vairbale)
+        xDual[i] = dual::DualNumber(x[i], 0.0);
     }
 
     // calculate the partial derivative of  each dimension
     for (int i = 0; i < DIM; ++i) {
-      xDual[i].dual = 1.0; // derivative w.r.t. dimension i
-      dual::DualNumber result =
-        Function::evaluate(xDual); // evaluate the function using AD
-      gradient[i] = result.dual;   // store derivative
-      // printf("\nxDual[%d]: %f, grad[%d]: %f ",i,xDual[i].real,i,gradient[i]);
-      xDual[i].dual = 0.0;
+        xDual[i].dual = 1.0; // derivative w.r.t. dimension i
+        dual::DualNumber result = Function::evaluate(xDual); // evaluate the function using AD
+        gradient[i] = result.dual; // store derivative
+        //printf("\nxDual[%d]: %f, grad[%d]: %f ",i,xDual[i].real,i,gradient[i]);
+        xDual[i].dual = 0.0;
     }
-  } // end calculate gradientusingAD
+}//end calculate gradientusingAD
+
 
 } // end of dual
+
--- include/pso.cuh
+++ include/pso.cuh
@@ -3,87 +3,83 @@
 
 namespace pso {
 
-  // kernel #1: initialize X, V, pBest; atomically seed gBestVal/gBestX
-  template <typename Function, int DIM>
-  __global__ void
-  psoInitKernel(Function func,
-                double lower,
-                double upper,
-                double* X,
-                double* V,
-                double* pBestX,
-                double* pBestVal,
-                double* gBestX,
-                double* gBestVal,
-                int N,
-                uint64_t seed,
-                curandState* states)
-  {
+// kernel #1: initialize X, V, pBest; atomically seed gBestVal/gBestX
+template<typename Function, int DIM>
+__global__ void psoInitKernel(
+    Function           func,
+    double             lower,
+    double             upper,
+    double*            X,
+    double*            V,
+    double*            pBestX,
+    double*            pBestVal,
+    double*            gBestX,
+    double*            gBestVal,
+    int                N,
+    uint64_t           seed,
+    curandState*       states)
+{
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i >= N)
-      return;
+    if (i >= N) return;
 
     curandState localState = states[i];
     const double vel_range = (upper - lower) * 0.1;
     // const unsigned int seed = 1234u;
-    // uint64_t counter = seed ^ (uint64_t)i;
-    // uint64_t state = seed * 0x9e3779b97f4a7c15ULL + (uint64_t)i;
-    // if (i==0) {
+    //uint64_t counter = seed ^ (uint64_t)i;
+    //uint64_t state = seed * 0x9e3779b97f4a7c15ULL + (uint64_t)i;
+    //if (i==0) {
     //  printf(">> initKernel sees seed = %llu\n", (unsigned long long)seed);
     //}
     // init position & velocity
     for (int d = 0; d < DIM; ++d) {
-      double rx = util::generate_random_double(&localState, lower, upper);
-      double rv =
-        util::generate_random_double(&localState, -vel_range, vel_range);
+        double rx = util::generate_random_double(&localState,lower, upper);
+        double rv = util::generate_random_double(&localState,-vel_range, vel_range);
 
-      X[i * DIM + d] = rx;
-      V[i * DIM + d] = rv;
-      pBestX[i * DIM + d] = rx;
+        X[i*DIM + d]      = rx;
+        V[i*DIM + d]      = rv;
+        pBestX[i*DIM + d] = rx;
     }
 
     // eval personal best
-    double fval = Function::evaluate(&X[i * DIM]);
+    double fval = Function::evaluate(&X[i*DIM]);
     pBestVal[i] = fval;
 
     // atomic update of global best
     double oldGB = util::atomicMinDouble(gBestVal, fval);
     if (fval < oldGB) {
-      // we’re the new champion: copy pBestX into gBestX
-      for (int d = 0; d < DIM; ++d)
-        gBestX[d] = pBestX[i * DIM + d];
+        // we’re the new champion: copy pBestX into gBestX
+        for (int d = 0; d < DIM; ++d)
+            gBestX[d] = pBestX[i*DIM + d];
     }
     states[i] = localState; // next time we draw, we continue where we left off
-  }
+}
 
-  // kernel #2: one PSO iteration (velocity+position update, personal & global
-  // best)
-  template <typename Function, int DIM>
-  __global__ void
-  psoIterKernel(Function func,
-                double lower,
-                double upper,
-                double w,  // weight inertia
-                double c1, // cognitive coefficient
-                double c2, // social coefficient
-                double* X,
-                double* V,
-                double* pBestX,
-                double* pBestVal,
-                double* gBestX,
-                double* gBestVal,
-                double* traj, // pass nullptr if not saving
-                bool saveTraj,
-                int N,
-                int iter,
-                uint64_t seed,
-                curandState* states) //,
-  // Result<DIM>&        best) // store the best particle's results at each
-  // iteration
-  {
+
+// kernel #2: one PSO iteration (velocity+position update, personal & global best)
+template<typename Function, int DIM>
+__global__ void psoIterKernel(
+    Function           func,
+    double             lower,
+    double             upper,
+    double             w, // weight inertia
+    double             c1, // cognitive coefficient
+    double             c2, // social coefficient
+    double*            X,
+    double*            V,
+    double*            pBestX,
+    double*            pBestVal,
+    double*            gBestX,
+    double*            gBestVal,
+    double*            traj,        // pass nullptr if not saving
+    bool               saveTraj,
+    int                N,
+    int                iter,
+    uint64_t           seed,
+    curandState*       states)//,
+    //Result<DIM>&        best) // store the best particle's results at each iteration
+{
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i >= N)
-      return;
+    if (i >= N) return;
 
     curandState localState = states[i];
     /*uint64_t state = seed;
@@ -92,50 +88,50 @@ namespace pso {
     */
     // update velocity & position
     for (int d = 0; d < DIM; ++d) {
-      // uint64_t z1 = splitmix64(state);
-      // uint64_t z2 = splitmix64(state);
-      // double r1 = random_double(state,0.0,1.0 );
-      // //util::generate_random_double(seed1, 0.0, 1.0); double r2 =
-      // random_double(state, 0.0, 1.0); //util::generate_random_double(seed2,
-      // 0.0, 1.0);
-      double r1 = util::generate_random_double(&localState, 0.0, 1.0);
-      double r2 = util::generate_random_double(&localState, 0.0, 1.0);
+        //uint64_t z1 = splitmix64(state);
+        //uint64_t z2 = splitmix64(state);
+        //double r1 = random_double(state,0.0,1.0 ); //util::generate_random_double(seed1, 0.0, 1.0);
+        //double r2 = random_double(state, 0.0, 1.0); //util::generate_random_double(seed2, 0.0, 1.0);
+        double r1 = util::generate_random_double(&localState, 0.0, 1.0);
+        double r2 = util::generate_random_double(&localState, 0.0, 1.0);
 
-      double xi = X[i * DIM + d];
-      double vi = V[i * DIM + d];
-      double pb = pBestX[i * DIM + d];
-      double gb = gBestX[d];
+        double xi = X[i*DIM + d];
+        double vi = V[i*DIM + d];
+        double pb = pBestX[i*DIM + d];
+        double gb = gBestX[d];
 
-      double nv = w * vi +
-                  c1 * r1 * (pb - xi) // “cognitive” pull toward personal best
-                  + c2 * r2 * (gb - xi); // “social” pull toward global best
-      double nx = xi + nv;
+        double nv = w*vi
+                  + c1*r1*(pb - xi) // “cognitive” pull toward personal best
+                  + c2*r2*(gb - xi); // “social” pull toward global best
+        double nx = xi + nv;
 
-      V[i * DIM + d] = nv;
-      X[i * DIM + d] = nx;
+        V[i*DIM + d] = nv;
+        X[i*DIM + d] = nx;
 
-      if (saveTraj) {
-        // traj is laid out [iter][i][d]
-        size_t idx = size_t(iter) * N * DIM + i * DIM + d;
-        traj[idx] = nx;
-      }
+        if (saveTraj) {
+            // traj is laid out [iter][i][d]
+            size_t idx = size_t(iter)*N*DIM + i*DIM + d;
+            traj[idx] = nx;
+        }
     }
 
+
+
     // evaluate at new position
-    double fval = Function::evaluate(&X[i * DIM]);
+    double fval = Function::evaluate(&X[i*DIM]);
 
     // personal best? no atomic needed, it's a private best position
     if (fval < pBestVal[i]) {
-      pBestVal[i] = fval;
-      for (int d = 0; d < DIM; ++d)
-        pBestX[i * DIM + d] = X[i * DIM + d];
+        pBestVal[i] = fval;
+        for (int d = 0; d < DIM; ++d)
+            pBestX[i*DIM + d] = X[i*DIM + d];
     }
 
     // global best?
     double oldGB = util::atomicMinDouble(gBestVal, fval);
     if (fval < oldGB) {
-      for (int d = 0; d < DIM; ++d)
-        gBestX[d] = X[i * DIM + d];
+        for (int d = 0; d < DIM; ++d)
+            gBestX[d] = X[i*DIM + d];
     }
     // best.coordinates = gBestX;
     // best.fval = gBestVal;
@@ -144,6 +140,12 @@ namespace pso {
         printf(" %8.4f", gBestX[d]);
     printf(" ]\n");*/
     states[i] = localState; // next time we draw, we continue where we left off
-  }
+}
+
+
+
+
+
 
 }
+
