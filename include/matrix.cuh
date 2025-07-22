a470f156c11613978a006a5bc4a47390d9c0c91c
Dominik Soós <soos.domi@gmail.com>
2025-07-22 11:22:10 -0400

    implementation + how the user would use matrix lib

---
+++ include/matrix.cuh
@@ -0,0 +1,70 @@
+#pragma once
+#include <cuda_runtime.h>
+#include <algorithm>
+#include <cstddef>
+#include <stdexcept>
+#include <cstdio>
+#include <cstdlib>
+
+template<typename T>
+class Matrix {
+private:
+  T* host_data_   = nullptr;
+  T* device_data_ = nullptr;
+  std::size_t rows_ = 0, cols_ = 0;
+
+public:
+  // default constructor
+  Matrix() noexcept = default;
+
+  // allocate separate host & device buffers
+  Matrix(std::size_t rows, std::size_t cols)
+    : rows_(rows), cols_(cols)
+  {
+    if (!rows_ || !cols_)
+      throw std::invalid_argument("Matrix dimensions must be > 0");
+
+    host_data_ = static_cast<T*>(std::malloc(rows_*cols_*sizeof(T)));
+    if (!host_data_)
+      throw std::runtime_error("host malloc failed");
+
+    cudaError_t err = cudaMalloc(&device_data_, rows_*cols_*sizeof(T));
+    if (err != cudaSuccess)
+      throw std::runtime_error("cudaMalloc failed");
+
+    printf("Matrix: allocated host & device memory\n");
+  }
+
+  // picks the right pointer on host vs. device
+  __host__ __device__
+  T* data() {
+  #ifdef __CUDA_ARCH__
+    return device_data_;
+  #else
+    return host_data_;
+  #endif
+  }
+  
+  __host__ __device__
+  T const* data() const {
+  #ifdef __CUDA_ARCH__
+    return device_data_;
+  #else
+    return host_data_;
+  #endif
+  }
+
+  __host__ __device__
+  T& operator()(std::size_t i, std::size_t j) {
+    return data()[i*cols_ + j];
+  }
+  
+  __host__ __device__
+  T const& operator()(std::size_t i, std::size_t j) const {
+    return data()[i*cols_ + j];
+  }
+
+  std::size_t rows() const noexcept { return rows_; }
+  std::size_t cols() const noexcept { return cols_; }
+
+};
