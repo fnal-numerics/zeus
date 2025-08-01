e9a360eea31a2e038f543a1a418ff3785a2a7d42
Dominik Soós <soos.domi@gmail.com>
2025-08-01 19:18:50 -0400

    ctor + copy_to_host tests

--- testing/cuda_buffer_tests.cu
+++ testing/cuda_buffer_tests.cu
@@ -9,4 +9,10 @@ TEST_CASE("Allocation of small buffer succeeds", "[cuda_buffer]") {
     REQUIRE(buf.data() != nullptr);
 }
 
+TEST_CASE("Constructor from host array + copy_to_host round-trips", "[cuda_buffer]") {
+    std::array<double,3> host = {{1.1,2.2,3.3}};
+    cuda_buffer buf(host);
+    auto out = buf.copy_to_host();
+    REQUIRE(out == std::vector<double>(host.begin(), host.end()));
+}
 
