02380cb502bda86cfe40b6e91bd08da7b426314f
Dominik Soós <soos.domi@gmail.com>
2025-08-15 11:31:30 -0400

    corrected device matrix

--- include/device_matrix.cuh
+++ include/device_matrix.cuh
@@ -6,7 +6,7 @@
   This class is for device allocation only from GPU heap memory
 */
 template <typename T>
-class DeviceMatrix:
+class DeviceMatrix {
 private:
   T* data_ = nullptr;
   std::size_t cols_ = 0;
@@ -15,35 +15,36 @@ private:
 public:
   __device__
   DeviceMatrix(std::size_t rows, std::size_t cols)
-  : data_(nullptr), rows_(rows), cols_(cols) 
+  : data_(nullptr), rows_(rows), cols_(cols)
   {
-    size_t sz = rows_ * cols_ * sizeof(T);
-    data_ = (T*)malloc(sz);
-    if(!data_) asm("trap;");
+    const std::size_t sz = rows_*cols_*sizeof(T);
+    data_ = static_cast<T*>(malloc(sz));
+    if (!data_) asm("trap;");
   }
 
-  DeviceMatrix(Matrix const&) = delete; // delete copy constructor
-  DeviceMatrix& operator=(Matrix const&) = delete; // delete lvalue assign completely
+  DeviceMatrix(const DeviceMatrix&) = delete; // delete copy constructor
+  DeviceMatrix& operator=(const DeviceMatrix&) = delete; // delete lvalue assign completely
 
   __device__
   ~DeviceMatrix() {
     if(data_) free(data_);
   }
 
-  __device__ T const*
-  data() const { return data_; }
+  __device__  T* data() { return data_; }
+  __device__ const T* data() const { return data_; }
 
-  __device__ T*
-  data() const { return data_; }
+  __device__ std::size_t rows() const { return rows_; }
+  __device__ std::size_t cols() const { return cols_; }
 
- __device__ T&
- operator()(std::size_t i, std::size_t j) {
-   return data()[i * cols_ + j];
- }
+  __device__ T& operator()(std::size_t i, std::size_t j) {
+    return data_[i*cols_ + j];
+  }
+  __device__ const T& operator()(std::size_t i, std::size_t j) const {
+    return data_[i*cols_ + j];
+  }
 
- __device__ T const&
- operator()(std::size_t i, std::size_t j) const {
-   return data()[i * cols_ + j];
- }
+  __device__ void set(std::size_t i, std::size_t j, const T& x) {
+    data_[i*cols_ + j] = x;
+  }
 
-
+};
