63fa6b7952bf499ae765cfc72e0824c2d690f615
Marc Paterno <paterno@fnal.gov>
2026-02-10 20:09:57 -0600

    Add Documentation To BFGSContext
    Document the BFGSContext struct and its member variables to
    clarify their roles in coordinating parallel BFGS optimizations.
    Remove unused cuda_runtime.h include.

--- include/context.cuh
+++ include/context.cuh
@@ -1,10 +1,11 @@
 #pragma once
 
-#include <cuda_runtime.h>
-
 namespace util {
+  /// Shared state for coordinating parallel BFGS optimizations.
+  /// Tracks global convergence status and signals early termination
+  /// when required number of optimizations have converged.
   struct BFGSContext {
-    int stopFlag;
-    int convergedCount;
+    int stopFlag;        ///< Global flag to signal all threads to stop
+    int convergedCount;  ///< Number of optimizations that have converged
   };
 }
